5f0ca061f1edfc6da960ca5d2d35fe4a78302bc5
zven21 <andzven@gmail.com>
2020-10-16 09:29:23 +0800

    init

---
+++ lib/bank_app.ex
@@ -0,0 +1,18 @@
+defmodule BankApp do
+  @moduledoc """
+  Documentation for `BankApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BankApp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
